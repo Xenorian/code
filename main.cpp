2e27ba7e90c04f8f7fcd7680c0b6e6eece88ce42
jsc <565405659@qq.com>
2023-02-15 16:38:34 +0800

    1

--- main.cpp
+++ main.cpp
@@ -334,7 +334,7 @@ int main(int argc, const char **argv) {
     if (show_another_window) {
       ImGui::Begin("Another Window",
                    &show_another_window);  // Pass a pointer to our bool variable (the window will have a closing button
-      // that will clear the bool when clicked)
+                                           // that will clear the bool when clicked)
       ImGui::Text("Hello from another window!");
       if (ImGui::Button("Close Me")) show_another_window = false;
       ImGui::End();
